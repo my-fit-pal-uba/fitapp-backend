4305b7d5a80982490531135b1dcce3589cd78b33
Jignacio14 <j14castro2001@gmail.com>
2025-03-24 20:33:47 -0300

    feat(Base file): Extraigo puerto del system

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5000
 
+ENV PORT=5000
+
 CMD ["python", "app.py"]
--- src/app.py
+++ src/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import os 
 
 app = Flask(__name__)
+
+DEFAULT_PORT = '8080'
 
 @app.route('/')
 def home():
     return "¡Hola, Docker con Flask!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        port_data = os.getenv('PORT', DEFAULT_PORT)
+        port = int(port_data)
+        app.run(host='0.0.0.0', port=port_data)
+    except Exception as e:
+        print(f"Error: {e}")
+
+    
