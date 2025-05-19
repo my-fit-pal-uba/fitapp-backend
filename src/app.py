26c188b97b611f65ff45852004419f3a5f4c0732
Jignacio14 <j14castro2001@gmail.com>
2025-05-19 19:34:43 -0300

    feat: change main app

--- src/app.py
+++ src/app.py
@@ -2,30 +2,37 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  #
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from datetime import datetime
 from flask import Flask
 from flask_cors import CORS
-import os
 from access_module.routes.login_controller import login_bp
-
-app = Flask(__name__)
-CORS(app, origins=["http://localhost:8081"], supports_credentials=True)
-app.register_blueprint(login_bp)
 
 DEFAULT_PORT = "8080"
 
 
-@app.route("/")
-def health_check():
-    return str(datetime.now())
+class BackendApp:
+    def __init__(self):
+        self.app = Flask(__name__)
+        CORS(self.app, origins=["http://localhost:8081"], supports_credentials=True)
+        self.app.register_blueprint(login_bp)
+        self.register_routes()
+
+    def register_routes(self):
+        @self.app.route("/")
+        def health_check():
+            return str(datetime.now())
+
+    def run(self):
+        try:
+            port_data = os.getenv("PORT", DEFAULT_PORT)
+            port = int(port_data)
+            self.app.run(host="0.0.0.0", port=port)
+        except Exception as e:
+            print(f"Error: {e}")
 
 
 if __name__ == "__main__":
-    try:
-        port_data = os.getenv("PORT", DEFAULT_PORT)
-        port = int(port_data)
-        app.run(host="0.0.0.0", port=port)
-    except Exception as e:
-        print(f"Error: {e}")
+    backend_app = BackendApp()
+    backend_app.run()
