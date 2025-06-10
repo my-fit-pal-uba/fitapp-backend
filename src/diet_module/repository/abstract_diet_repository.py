2f639347bc14fb9196cbabe2a6d1c3c5ea564be5
GianLucaSpagnolo <gianluuba@gmail.com>
2025-06-10 04:43:09 -0300

    feat: abstract diet repository first endpoints

---
+++ src/diet_module/repository/abstract_diet_repository.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+
+class AbstractDietRepository:
+    @abstractmethod
+    def get_diets(self):
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+    @abstractmethod
+    def get_diet_by_id(self, diet_id: int):
+        raise NotImplementedError("This method should be overridden by subclasses")
+    
+    @abstractmethod
+    def post_diet(self, diet):
+        raise NotImplementedError("This method should be overridden by subclasses")
+    
+    @abstractmethod
+    def remove_diet(self, diet_id: int):
+        raise NotImplementedError("This method should be overridden by subclasses")
+    
